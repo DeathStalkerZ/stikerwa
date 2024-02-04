fdd1e3e5b9197478673cbe46e693830b6792f647
DeathStalkerZ <49668367+DeathStalkerZ@users.noreply.github.com>
2024-02-04 13:55:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ create().then((client) => start(client));
 async function start(client) {
   client.onMessage(async (msg) => {
     // console.log(msg);
+
+    if (msg.body === "Menu" || msg.body === "menu" || msg.body === "P" || msg.body === "p" || msg.body === ".menu" || msg.body === ".sticker" || msg.body === ".stiker" || msg.body === "Bot" || msg.body === "bot" || msg.body === "!menu") {
+      client.sendText(msg.from, `
+    *Halo Selamat Datang di Bot Ahza*
+    !cek = Untuk cek bot aktif atau engga
+    !stiker = Buat stiker pake gambar/video
+    !rules = cek peraturan bot ini`);
+      client;
+    }
+  
+    if (msg.body === "!stiker") {
+      client.sendText(msg.from, "Sertakan Dengan Gambar/Video dan Caption !stiker");
+      client;
+    }
+
     if (msg.body === "!cek") {
       client.sendText(msg.from, "👋 Hello! BOT AKTIF");
       client;
     }
-	if (msg.body === "!cmd") {
-      client.sendText(msg.from, `
-	  !cek = Untuk cek bot aktif atau engga
-	  !jadwal = Menunjukan Matkul 
-	  !stiker = Buat stiker pake gambar
-	  !rules = cek peraturan bot ini`);
-      client;
-    }
+
 	if (msg.body === "!rules") {
       client.sendText(msg.from, ` 
 	Rules ... !!!
@@ -44,16 +52,13 @@ bot ini
 	`);
       client;
     }
-	if (msg.body === "P") {
-      client.sendText(msg.from, "Iya?");
-      client;
-    }
-	if (msg.body === "!test") {
+
+	if (msg.body === "Test" || msg.body === "test" || msg.body === "Hi" || msg.body === "Hallo") {
       client.sendText(msg.from, `
-		*SELAMAT DATANG DI BOT Ahza ^_^* 
+		*SELAMAT DATANG DI BOT AHZA ^_^* 
 
 _Hai kawan, sebelum memakai bot ini patuhi rules dulu ya ._
-Ketikan *!rules* untuk melihat rules memakai bot ini`);
+Ketikan *!rules* untuk melihat rules memakai bot ini, dan *!menu* untuk melihat menu bot ini`);
       client;
     }
 	if (msg.body === "Ahza" || msg.body === "ahza") {
@@ -61,63 +66,19 @@ Ketikan *!rules* untuk melihat rules memakai bot ini`);
       client;
     }
 	
-	
-	if (msg.body === "!jadwal") {
-      client.sendText(msg.from, `
-				✨--JADWAL MATKUL--✨
- ----------------------------------------------	
-👉Senin 13.30-15.10 - Teknologi Antar Jaringan
-👨Dosen = Mulkan Azhari, M.kom
 
-👉Senin 15.10-16.50 - Managemen Proyek TI  
-👨Dosen = Budi Kurniawan, M.Kom
-
-👉Senin 16.50-18.30 - Keamanan Informasi 
-👨Dosen = Farid Akbar M.Kom 
- ----------------------------------------------
- ----------------------------------------------
-👉Selasa 13.30-.16.00 - Kecerdasan Buatan 
-👨Dosen = Al-Khowarizmi, M.Kom
-
-👉Selasa 16.00-17.30 - Prak. Statistik dan Probabilitas 
-👩Dosen = Hevlie Winda Nazry S, S.pd, M.Si
- -----------------------------------------------
- -----------------------------------------------
-👉Rabu 13.30-15.10 - Islam dan Iptek 
-👩Dosen = Nadlrah Naimi, M.A
-
-👉Rabu 15.10-16.50 - Sistem Mikroprosesor dan Assembly 
-👨Dosen = Muhammad Basri M.Kom
- -----------------------------------------------
- -----------------------------------------------
-👉Kamis 13.30-15.10 - Statistik dan Probabilitas 
-👩Dosen = Hevlie Winda Nazry S, S.pd, M.Si
- 
-👉Kamis 15.10-16.50 - Pengelolaan Citra Digital
-👩Dosen = Indah Purnama Sari, M.Kom
- ------------------------------------------------
- ------------------------------------------------
-👉Jum'at 14.50-16.20 - Prak. Pengelolaan Citra Digital
-👩Dosen = Indah Purnama Sari, M.Kom
-
-👉Juma'at 16.30-18.00 - Prak. Teknologi Antar Jaringan
-👨Dosen = Mulkan Azhari, M.kom
- ------------------------------------------------
- ------------------------------------------------
-👉Sabtu 16.00-17.30 - Prak. Sistem Mikroprosesor dan Assembly 
-👨Dosen = Muhammad Basri M.Kom
-
-`);
-      client;
+  if (msg.mimetype) {
+      if (msg.caption === "!stiker" && msg.type === "video") {
+        const mediaData = await decryptMedia(msg);
+        const imageBase64 = `data:${msg.mimetype};base64,${mediaData.toString(
+          "base64"
+        )}`;
+        await client.sendMp4AsSticker(msg.from, imageBase64);
+      }
     }
-    
 
 
-
-
-
-
-	else if (msg.mimetype) {
+	if (msg.mimetype) {
       if (msg.caption === "!stiker" && msg.type === "image") {
         const mediaData = await decryptMedia(msg);
         const imageBase64 = `data:${msg.mimetype};base64,${mediaData.toString(
